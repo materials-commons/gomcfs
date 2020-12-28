f9d36eca64df7a5f59ec7364cc4578690da7026f
gtarcea <glenn.tarcea@gmail.com>
2020-12-28 10:59:50 -0500

    WIP - Directory Listing

--- fs/node.go
+++ fs/node.go
@@ -1,16 +1,22 @@
 package fs
 
 import (
+	"context"
+	"hash/fnv"
 	"os/user"
 	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/hanwen/go-fuse/v2/fuse"
+	"github.com/materials-commons/gomcfs/mcapi"
 )
 
 type Node struct {
 	fs.Inode
-	APIToken string
-	MCURL    string
+	mcapi  mcapi.Client
+	MCFile *mcapi.MCFile
 }
 
 // Set file owners to the current user,
@@ -26,4 +32,51 @@ func init() {
 	gid32, _ := strconv.ParseUint(u.Gid, 10, 32)
 	uid = uint32(uid32)
 	gid = uint32(gid32)
+}
+
+func (n *Node) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
+	path := n.Path(n.Root())
+	files, err := n.mcapi.ListDirectory(path)
+	if err != nil {
+		return nil, syscall.EIO
+	}
+
+	filesList := make([]fuse.DirEntry, 0, len(files))
+
+	for _, fileEntry := range files {
+		entry := fuse.DirEntry{
+			Mode: n.getMode(fileEntry),
+			Name: fileEntry.Name,
+			Ino:  n.inodeHash(fileEntry),
+		}
+
+		filesList = append(filesList, entry)
+	}
+
+	return fs.NewListDirStream(filesList), fs.OK
+}
+
+func (n *Node) Getattr(ctx context.Context, out *fuse.AttrOut) syscall.Errno {
+	out.Mode = n.getMode(*n.MCFile)
+	out.Size = n.MCFile.Size
+	out.Ino = n.inodeHash(*n.MCFile)
+	now := time.Now()
+	out.SetTimes(&now, &now, &now)
+	out.Uid = uid
+	out.Gid = gid
+	return fs.OK
+}
+
+func (n *Node) getMode(entry mcapi.MCFile) uint32 {
+	if entry.IsDir() {
+		return 0755 | uint32(syscall.S_IFDIR)
+	}
+
+	return 0644 | uint32(syscall.S_IFREG)
+}
+
+func (n *Node) inodeHash(entry mcapi.MCFile) uint64 {
+	h := fnv.New64a()
+	_, _ = h.Write([]byte(entry.FullPath()))
+	return h.Sum64()
 }
--- mcapi/models.go
+++ mcapi/models.go
@@ -2,9 +2,10 @@ package mcapi
 
 type File struct {
 	ID          int    `json:"id"`
+	Name        string `json:"name"`
 	Path        string `json:"path"`
 	DirectoryID int    `json:"directory_id"`
-	Size        int64  `json:"size"`
+	Size        uint64 `json:"size"`
 	Checksum    string `json:"checksum"`
 	MimeType    string `json:"mime_type"`
 }
@@ -20,4 +21,19 @@ func (f File) IsFile() bool {
 
 func (f File) IsDir() bool {
 	return f.MimeType == "directory"
+}
+
+func (f MCFile) FullPath() string {
+	if f.IsDir() {
+		if f.Path == "/" {
+			return f.Path + f.Name
+		}
+		return f.Path + "/" + f.Name
+	}
+
+	if f.Directory.Path == "/" {
+		return f.Directory.Path + f.Name
+	}
+
+	return f.Directory.Path + "/" + f.Name
 }
